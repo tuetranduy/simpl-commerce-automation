d1f53ce75c83331e28a3e5fe13ccac5c0cb7f7ac
Tue Tran <tue1996@gmail.com>
2026-02-01 12:58:21 +0700

    fix: Update Playwright reporter configuration and adjust viewport settings for CI

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,10 @@ export default defineConfig({
   fullyParallel: false,
   forbidOnly: !!process.env.CI,
   workers: 1,
-  reporter: [["html", { outputFolder: "playwright-report" }], ["list"]],
+  reporter: [
+    ["html", { outputFolder: "playwright-report", open: "never" }],
+    ["list"],
+  ],
   use: {
     baseURL: "https://demo.simplcommerce.com/",
     trace: "on-first-retry",
@@ -18,7 +21,12 @@ export default defineConfig({
   projects: [
     {
       name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
+      use: {
+        viewport: process.env.CI ? { width: 1920, height: 1080 } : null,
+        launchOptions: {
+          args: ["--start-maximized"],
+        },
+      },
     },
   ],
 });
